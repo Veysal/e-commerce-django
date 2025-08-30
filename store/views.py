3a099dd59d531d261bfe292cfeed44c3c8582edc
Veysal <tveysal93@mail.ru>
2025-08-30 09:10:05 +0300

    Сделана отдельная страница для каждого товара

--- store/views.py
+++ store/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Product, Category
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    products = Product.objects.filter(is_available=True)[:8]
+    context = {'products': products}
+    return render(request, 'index.html', context)
 
 
+def product_detail(request, product_slug):
+    # get_object_or_404 либо находит объектб либо возвращает ошибку 404
+    product = get_object_or_404(Product, slug = product_slug,is_available = True)
+    context = {'product': product}
+    return render(request, 'product_detail.html', context)
