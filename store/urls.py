430604b42bbee9bde8094a3ae75123232e4a43b5
Veysal <tveysal93@mail.ru>
2025-08-30 10:01:32 +0300

    Добавили базовый шаблон. Создали шаблон с категорией товаров и с товарами определнной категории

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'store'
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('product/<slug:product_slug>', views.product_detail, name='product_detail'),
